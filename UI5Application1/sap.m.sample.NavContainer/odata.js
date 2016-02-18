a948e7153a969ae1431df2e52e6a9621d074a305
Broenne <marcus.broenstrup@gmail.com>
2016-02-18 20:04:02 +0100

    no message

---
+++ UI5Application1/sap.m.sample.NavContainer/odata.js
@@ -0,0 +1,17 @@
+﻿
+//"use strict";
+//function odataController(Controller) {
+//    "use strict";
+
+//    var Ctest = test.extend("sap.m.sample.NavContainer.C", {
+//        handleNav: function test() {
+//            console.log("odata");
+//            var oModel = new sap.ui.model.odata.ODataModel("http://localhost:2344/odata", false);
+//            console.log("oModel" + oModel);
+//            // catch callback???
+//            sap.ui.getCore().setModel(oModel);
+//        }
+//    });
+
+//    return Ctest;
+//}
---
+++ UI5Application1/ui5application1/Main.view.js
@@ -0,0 +1,18 @@
+sap.ui.jsview("ui5application1.Main", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf ui5application1.Main
+	*/ 
+	getControllerName : function() {
+		return "ui5application1.Main";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf ui5application1.Main
+	*/ 
+	createContent : function(oController) {
+
+	}
+});
